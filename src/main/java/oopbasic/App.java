9d0a910a8b06a3ded6e77514fdd16fdcc44b6064
tu4 <tu4e4ka096@gmail.com>
2016-04-11 18:48:09 +0300

    change app

--- src/main/java/oopbasic/App.java
+++ src/main/java/oopbasic/App.java
@@ -14,7 +14,15 @@ public class App {
         box2.height = 12;
         box2.widht = 12;
         box2.langht = 12;
+        Box box3 = new Box(15,15,20);
+        Box box4 = new Box();
+        Box box5 = new Box(150,150,20);
+
+
         System.out.println(box1.getVolume());
         System.out.println(box2.getVolume());
+        System.out.println(box3.getVolume());
+        System.out.println(box4.getVolume());
+        System.out.println(box5.getVolume());
     }
 }
--- src/main/java/oopbasic/Box.java
+++ src/main/java/oopbasic/Box.java
@@ -8,8 +8,20 @@ public class Box {
     int height;
     int langht;
 
+    Box(){
+        widht = 10;
+        height = 10;
+        langht = 10;
+    }
+
+    Box(int widht, int height, int langht){
+        this.widht = widht;
+        this.height = height;
+        this.langht = langht;
+    }
     public int getVolume(){
         return height * widht * langht;
     }
+
 }
 
